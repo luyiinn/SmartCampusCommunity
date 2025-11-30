a54f1e33521c808ccf6b5d49d78d877fa4340fa5
dewmark <1259133575@qq.com>
2025-11-30 19:34:40 +0800

    个人主页功能

---
+++ src/main/java/com/dewmark/smartcampuscommunity/controller/user/MyHomeController.java
@@ -0,0 +1,23 @@
+package com.dewmark.smartcampuscommunity.controller.user;
+
+import com.dewmark.smartcampuscommunity.pojo.vo.PageVO;
+import com.dewmark.smartcampuscommunity.pojo.vo.PostListVO;
+import com.dewmark.smartcampuscommunity.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: dewMark
+ * @date: 2025/11/27
+ **/
+@RestController
+@RequestMapping
+@Slf4j
+public class MyHomeController {
+
+//    public Result<PageVO<PostListVO>> postList(String keyword) {
+//
+//    }
+}
